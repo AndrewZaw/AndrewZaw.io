b45fafb1f0509f7e173b83eb9242f4a397ed3f63
azaw502 <andrew502502@gmail.com>
2019-05-18 16:41:28 -0400

    Added descriptions

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -16,3 +16,13 @@ $(document).ready(function() {
 $(".collapse").collapse({
   toggle: false
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
