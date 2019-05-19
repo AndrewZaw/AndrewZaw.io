5a149c9864bf6b8b7d96c7242d54cb72e99a6a3a
azaw502 <andrew502502@gmail.com>
2019-05-19 16:20:29 -0400

    Update 2019-05-19T20:20:28.484Z

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,3 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-
 function copyToClipboardEmail() {
   var email = document.getElementById("email");
   email.select();
@@ -27,14 +24,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth"
     });
-  });
-});
-
-$(function() {
-  AOS.init({
-    once: true,
-    offset: 100,
-    easing: "ease-out",
-    duration: 400
   });
 });
